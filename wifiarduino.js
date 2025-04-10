dd47c8bffa9723bcf2e6d46e8390cc9f8b716f7f
Rodrigo Montero <43457014+monteroCastillo@users.noreply.github.com>
2025-04-10 03:17:06 -0500

    Update wifiarduino.js

--- wifiarduino.js
+++ wifiarduino.js
@@ -8,6 +8,10 @@ app.post("/sensor-data", (req, res) => {
     res.send("Datos recibidos correctamente.");
 });
 
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor ejecutándose en el puerto ${port}`);
